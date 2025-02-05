ed7429347062a1a94d86a5c68a9b77315928dd77
huanglei <3257752854@qq.conm>
2025-02-05 15:59:35 +0800

    allow host

--- online/online/settings.py
+++ online/online/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-t$^!&5f$3@5&ft646xjztlh)-71qfr5gpodtq$mcsyp^hkvuf&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['121.43.131.90', 'localhost', '127.0.0.1']
 
 
 # Application definition
